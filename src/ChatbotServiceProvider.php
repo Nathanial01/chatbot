93b44c8ecc4e2ae8e916d583854d06cfb5d435aa
Nathanial01 <9011034@student.zadkine.nl>
2024-11-08 10:40:03 +0100

    updatedatabase

--- src/ChatbotServiceProvider.php
+++ src/ChatbotServiceProvider.php
@@ -8,7 +8,7 @@ class ChatbotServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations/CreateChatHistoriesTable');
 
         $this->publishes([
             __DIR__ . '/../database/migrations' => database_path('migrations'),
